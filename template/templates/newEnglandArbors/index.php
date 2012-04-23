287a88a840008e4d7e021b9c06880aa6784e612e
Matt Elliott <melliott@ccistudios.com>
2012-04-23 11:03:26 -0400

    change

--- template/templates/newEnglandArbors/index.php
+++ template/templates/newEnglandArbors/index.php
@@ -87,7 +87,7 @@ $analytics = "UA-XXXXX-X"; // FIXME Update to client ID
 		<div class="clear"></div>
 		<?php if ($this->countModules('contentBottom')): ?>
 		<div id="contentBottom">
-			<jdoc:include type="modules" name="contentBottom" style="xhtml" />
+			<jdhoc:include type="modules" name="contentBottom" style="xhtml" />
 			<div class="clear"></div>
 		</div>
 		<?php endif; ?>
