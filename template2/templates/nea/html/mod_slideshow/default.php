64a48bcb54deef03498673baad691b55c7e5d660
Jonathan Bennett <jbennett@ccistudios.com>
2012-05-30 14:50:07 -0400

    Slideshow updates

--- template2/templates/nea/html/mod_slideshow/default.php
+++ template2/templates/nea/html/mod_slideshow/default.php
@@ -7,7 +7,12 @@
 		<? $first = true;
 		foreach($slides as $index => $slide): ?>
 		<div class="slide <?php if ($first) { echo 'active'; $first = false; } ?>"><div>
-			<? if ($slide->description1) { echo "<div class=\"description\"><div>{$slide->description1}</div></div>"; } ?>
+			<? if ($slide->description1): ?>
+				<div class="description"><div>
+					<h2><?= $slide->title ?></h2>
+					<?= $slide->description1 ?>
+				</div></div>
+			<? endif; ?>
 			<? if ($slide->image1) { echo "<div class=\"image\"><div><img src=\"{$imagePath}{$slide->image1}\" alt=\"\" /></div></div>"; } ?>
 		</div></div>
 		<? endforeach; ?>
@@ -16,7 +21,7 @@
 	<div class="thumbs"><div><div>
 		<? $first = true;
 		foreach($slides as $index => $slide): ?>
-		<div class="thumb <?php if ($first) { echo 'active'; $first = false; } ?>"><div><div>
+		<div class="thumb <?php if ($first) { echo 'active'; $first = false; } ?>" style="width: <?= 100/count($slides) ?>%;"><div><div>
 			<div class="title"><?= $slide->title ?></div>
 			<div class="preview"><img src="<?= "{$imagePath}{$slide->image2}" ?>" /></div>
 			<div class="indicator"></div>
@@ -28,10 +33,6 @@
 <script>
 window.addEvent('domready', function() {
 	var slideshow = new CCI.Slideshow('slideshow-<?= $moduleID ?>', <?= json_encode($options) ?>);
-	<? if ($autoplay): ?>
-	slideshow.addEvent('ready', function () {
-		slideshow.start();
-	});
-	<? endif; ?>
+	slideshow.play();
 });
 </script>
--- template2/templates/nea/javascript/com_slideshow.js
+++ template2/templates/nea/javascript/com_slideshow.js
@@ -1,5 +1,116 @@
-window.addEvent('domready', function () {
+var CCI = CCI || {};
+CCI.Slideshow = new Class({
+	Implements: [Options],
 
+	options: {
+		duration: 300,
+		delay: 5000
+	},
 
+	container: null,
+	tallestSlide: null,
+	slides: [],
+	thumbs: [],
+	current: null,
+	playing: false,
+	timer: null,
+
+	initialize: function(container, options) {
+		this.setOptions(options);
+		this.container = $(container);
+
+		this.slides = this.container.getElements('.slide');
+		this.thumbs = this.container.getElements('.thumb');
+
+		this.current = 0;
+
+		this.slides.each(function(slide, index) {
+			if (!this.tallestSlide ||
+				this.tallestSlide.getSize().y < slide.getSize().y) {
+				this.tallestSlide = slide;
+			}
+
+			if (index == this.current) {
+				slide.setStyle('opacity', 1);
+			} else {
+				slide.setStyle('opacity', 0);
+			}
+		}.bind(this));
+
+		this.thumbs.each(function(thumb, index) {
+			thumb.addEvent('click', function() {
+				this.moveTo(index);
+			}.bind(this));
+		}.bind(this));
+
+		this.resizeHeight();
+		window.addEvent('resize', function() {
+			this.resizeHeight();
+		}.bind(this));
+	},
+
+	resizeHeight: function() {
+		var height = 0;
+		if (this.slides[this.current] != this.tallestSlide) {
+			this.tallestSlide.setStyles({
+				opacity: 1,
+				display: 'block'
+			});
+			height = this.tallestSlide.getSize().y;
+			this.tallestSlide.setStyles({
+				opacity: 0,
+				display: ''
+			});
+		} else {
+			height = this.tallestSlide.getSize().y;
+		}
+
+		this.slides.each(function(slide, index) {
+			if (slide != this.tallestSlide) {
+				slide.setStyle('height', height);
+			}
+		}.bind(this));
+	},
+
+	play: function() {
+		this.running = true;
+		this.timer = this.transition.bind(this).delay(this.options.delay);
+	},
+
+	stop: function() {
+		this.running = false;
+		clearTimeout(this.timer);
+	},
+
+	moveTo: function(index) {
+		clearTimeout(this.timer);
+		this.transition(index);	
+	},
+
+	transition: function(nextIndex) {
+		if (nextIndex === undefined) {
+			nextIndex = this.getNextIndex();
+		}
+
+		var nextSlide = this.slides[nextIndex],
+			nextThumb = this.thumbs[nextIndex];
+
+		this.slides.removeClass('active');
+		this.slides[this.current].tween('opacity', 0);
+		nextSlide.addClass('active');
+		nextSlide.tween('opacity', 1);
+
+		this.thumbs.removeClass('active');
+		nextThumb.addClass('active');
+		this.current = nextIndex;
+
+		if (this.running) {
+			this.timer = this.transition.bind(this).delay(this.options.delay);
+		}
+	},
+
+	getNextIndex: function() {
+		return (this.current + 1) % this.slides.length;
+	}
 
 });
