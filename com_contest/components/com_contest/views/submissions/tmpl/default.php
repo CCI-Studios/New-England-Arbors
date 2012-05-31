dc527424f896b76f10634809a478f97f1226915c
Jonathan Bennett <jbennett@ccistudios.com>
2012-05-30 21:47:27 -0400

    Added body wrapper to page

--- com_contest/components/com_contest/views/submissions/tmpl/default.php
+++ com_contest/components/com_contest/views/submissions/tmpl/default.php
@@ -1,54 +1,57 @@
 <style src="/media/com_contest/css/com_contest.css" />
 <? @helper('behavior.modal') ?>
 
-<h1>Photo Contest</h1>
 
-<? if ($description): ?>
-	<p><?= $description ?></p>
-<? endif; ?>
+<div class="item-page">
+	<h1>Photo Contest</h1>
 
-<p><a href="<?= @route('view=submission&layout=form') ?>">Submit your own image.</a>
+	<? if ($description): ?>
+		<p><?= $description ?></p>
+	<? endif; ?>
 
-<div class="submission-list">
-	<?php foreach($submissions as $submission): ?>
-	<div class="item">
-		<p><a class="modal" href="/media/com_contest/uploads/<?= $submission->file ?>" rel="">
-			<img src="/media/com_contest/uploads/<?= $submission->file ?>" />
-		</a></p>
-		<p>
-			<strong><?= $submission->title ?></strong><br/>
-			Average Rating: <?= number_format($submission->rating, 1) ?>
-		</p>
+	<p><a href="<?= @route('view=submission&layout=form') ?>">Submit your own image.</a>
 
-		<? if(!$submission->hasRated()): ?>
-		<div class="rating">
-			<form action="<?= @route('view=rating') ?>" class="-koowa-form" method="post">
-				<input type="hidden" name="ip" value="<?= KRequest::get('server.REMOTE_ADDR', 'raw') ?>" />
-				<input type="hidden" name="contest_submission_id" value="<?= $submission->id ?>" />
-				<input type="hidden" name="action" value="save" />
+	<div class="submission-list">
+		<?php foreach($submissions as $submission): ?>
+		<div class="item">
+			<p><a class="modal" href="/media/com_contest/uploads/<?= $submission->file ?>" rel="">
+				<img src="/media/com_contest/uploads/<?= $submission->file ?>" />
+			</a></p>
+			<p>
+				<strong><?= $submission->title ?></strong><br/>
+				Average Rating: <?= number_format($submission->rating, 1) ?>
+			</p>
 
-				
-				<select name="rating">
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-				</select>
+			<? if(!$submission->hasRated()): ?>
+			<div class="rating">
+				<form action="<?= @route('view=rating') ?>" class="-koowa-form" method="post">
+					<input type="hidden" name="ip" value="<?= KRequest::get('server.REMOTE_ADDR', 'raw') ?>" />
+					<input type="hidden" name="contest_submission_id" value="<?= $submission->id ?>" />
+					<input type="hidden" name="action" value="save" />
 
-				<button>Rate Me</button>
-			</form>
+					
+					<select name="rating">
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+
+					<button>Rate Me</button>
+				</form>
+			</div>
+			<? endif; ?>
+
 		</div>
-		<? endif; ?>
-
+		<?php endforeach; ?>
 	</div>
-	<?php endforeach; ?>
-</div>
 
-<? if ($total > 10): ?>
-	<?= @helper('paginator.pagination', array(
-		'total' => $total,
-		'show_count' => false,
-		'show_limit' => false,
-	)) ?>
-<? endif; ?>
+	<? if ($total > 10): ?>
+		<?= @helper('paginator.pagination', array(
+			'total' => $total,
+			'show_count' => false,
+			'show_limit' => false,
+		)) ?>
+	<? endif; ?>
+</p>
