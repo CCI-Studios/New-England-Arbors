eba2f98c023f78a341eac0273a18c9dc879f9371
Matt Elliott <melliott@ccistudios.com>
2012-06-01 16:29:26 -0400

    zooming in and out turned off
    closes #99

--- template2/templates/nea/index.php
+++ template2/templates/nea/index.php
@@ -45,7 +45,7 @@ $analytics = "UA-31248154-1"; // FIXME Update to client ID
 
  	<jdoc:include type="head" />
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<link rel="icon" type="image/x-icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
 	<link rel="shortcut icon" type="image/x-icon" href="/templates/OAB/favicon.ico">	
 	<link rel="apple-touch-icon" href="/templates/<?= $this->template ?>/resources/apple-touch-icon.png">
