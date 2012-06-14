429ac73af4cdd24d4f59c021179a3e4481f05e33
Jonathan Bennett <jbennett@ccistudios.com>
2012-06-14 13:58:11 -0400

    Fixed html5 placeholder polyfill
    Closes #126

--- shopify/assets/image-select.js
+++ shopify/assets/image-select.js
@@ -10,3 +10,31 @@ window.addEvent('domready', function() {
 		large.set('src', this.get('data-large'));
 	});
 });
+
+(function (window, document) {
+	window.addEvent('load', function () {
+		if (Modernizr.input.placeholder) {
+			return;
+		}
+
+		$$('input[placeholder]').each(function (input) {
+			var placeholder = input.getProperty('placeholder');
+			if (input.value === '') {
+				input.value = placeholder;
+			}
+
+			input.addEvents({
+				focus: function () {
+					if (input.value === placeholder) {
+						input.value = '';
+					}
+				},
+				blur: function () {
+					if (input.value === '') {
+						input.value = placeholder;
+					}
+				}
+			});
+		});
+	});
+}(this, this.document));
