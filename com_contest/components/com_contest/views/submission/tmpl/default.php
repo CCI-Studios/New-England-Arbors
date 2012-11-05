fd71a6cd01c87f15d17681a930d41095258264ed
Jonathan Bennett <jbennett@ccistudios.com>
2012-11-05 14:41:12 -0500

    Typo

--- com_contest/components/com_contest/views/submission/tmpl/default.php
+++ com_contest/components/com_contest/views/submission/tmpl/default.php
@@ -1,6 +1,6 @@
 <div class="item-page">
 	<h1>Submission Received</h1>
 
-	<p>Thank you for submitting you photo.</p>
+	<p>Thank you for submitting your photo.</p>
 	<p><a href="<?= @route('view=submissions') ?>">Return to gallery.</a></p>
 </div>
